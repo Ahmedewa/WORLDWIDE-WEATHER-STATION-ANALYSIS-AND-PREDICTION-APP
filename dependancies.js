145b1974fb295ed982b73a088ccb864717839348
Ahmedewa <146005021+Ahmedewa@users.noreply.github.com>
2025-08-05 07:13:01 +0100

    Create             src/content/packages-dependancies.js
    Signed-off-by: Ahmedewa <146005021+Ahmedewa@users.noreply.github.com>

---
+++ dependancies.js
@@ -0,0 +1,29 @@
+                    ANGULAR [PACKAGE.JSON]DEPENDANCIES
+
+Angular uses `package.json` instead of `requirements.txt`. 
+`dependencies` section:
+
+```json
+"dependencies": {
+  "@angular/animations": "^18.2.1",
+  "@angular/common": "^18.2.1",
+  "@angular/compiler": "^18.2.1",
+  "@angular/core": "^18.2.1",
+  "@angular/forms": "^18.2.1",
+  "@angular/platform-browser": "^18.2.1",
+  "@angular/router": "^18.2.1",
+  "rxjs": "~7.8.0",
+  "zone.js": "~0.14.10",
+  "moment": "^2.30.1",
+  "axios": "^1.6.7"
+}
+```
+
+To install:
+```bash
+npm install
+```
+
+---
+
+## 🔁 2. Idempotency Design
